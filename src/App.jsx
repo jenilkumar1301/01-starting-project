f0dde41f0ea311e304c2be3dcfca595f6e6a6ca4
jenilkumar1301 <sarojpaghdal31@gmail.com>
2026-04-15 12:35:17 -0400

    fixed erros and normal  class work

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,34 @@
-import reactImg from './assets/react-core-concepts.png';
-
-
-
-function Header() {
- const reactdescription = ['Fundamental', 'crucial', 'core'];
-function genRandomInt(max) {
- return Math.floor(Math.random() * (max + 1));
-}
-const description = reactdescription[genRandomInt(2)];
-return(
-<header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-         {description} Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
-);
-}
+import { CORE_CONCEPTS } from './data.js';
+import Header from './components/header/header.jsx';
+import CoreConcept from './components/coreconcepts/coreconcepts.jsx';
+import TabButton from './components/TabButton/TabButton.jsx';
 function App() {
   return (
     <div>
-      <Header/> 
+      <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>Time to get started!</h2>
+          <ul>
+            <CoreConcept {...CORE_CONCEPTS[0]} />
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
+          </ul>
+        </section>
+
+        <section id="examples">
+          <h2>Examples</h2>
+          <ul>
+            <TabButton>Components</TabButton>
+            <TabButton>JSX</TabButton>
+            <TabButton>Props</TabButton>
+            <TabButton>State</TabButton>
+          </ul>
+        </section>
       </main>
     </div>
   );
 }
+
+export default App;
---
+++ src/data.js
@@ -0,0 +1,27 @@
+import componentsImg from './assets/components.png';
+import jsxImg from './assets/jsx-ui.png';
+import propsImg from './assets/config.png';
+import stateImg from './assets/state-mgmt.png';
+
+export const CORE_CONCEPTS = [
+  {
+    image: componentsImg,
+    title: 'Components',
+    description: 'The core UI building block in React.',
+  },
+  {
+    image: jsxImg,
+    title: 'JSX',
+    description: 'The syntax extension to describe user interfaces.',
+  },
+  {
+    image: propsImg,
+    title: 'Props',
+    description: 'Inputs that let components receive data.',
+  },
+  {
+    image: stateImg,
+    title: 'State',
+    description: 'Data that can change and update the UI.',
+  },
+];
