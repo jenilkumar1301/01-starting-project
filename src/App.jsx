d32d22971c0b54024466b8173e7f38b07a832383
jenilkumar1301 <sarojpaghdal31@gmail.com>
2026-04-22 11:32:00 -0400

    made changes in Tab button (in class)

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,31 @@
-import { CORE_CONCEPTS } from './data.js';
+import { CORE_CONCEPTS, EXAMPLES } from './data.js';
 import Header from './components/header/header.jsx';
 import CoreConcept from './components/coreconcepts/coreconcepts.jsx';
 import TabButton from './components/TabButton/TabButton.jsx';
+import { useState } from 'react';
+
 function App() {
+  const [selectedTopic, setSelectedTopic] = useState();
+  console.log('App is rendered');
+
+  function handleSelect(selectedButton) {
+    setSelectedTopic(selectedButton);
+  }
+
+  let tabContent = <p>Please select a topic.</p>;
+
+  if (selectedTopic) {
+    tabContent = (
+      <div id="tab-content">
+        <h3>{EXAMPLES[selectedTopic].title}</h3>
+        <p>{EXAMPLES[selectedTopic].description}</p>
+        <pre>
+          <code>{EXAMPLES[selectedTopic].code}</code>
+        </pre>
+      </div>
+    );
+  }
+
   return (
     <div>
       <Header />
@@ -19,12 +42,22 @@ function App() {
 
         <section id="examples">
           <h2>Examples</h2>
-          <ul>
-            <TabButton>Components</TabButton>
-            <TabButton>JSX</TabButton>
-            <TabButton>Props</TabButton>
-            <TabButton>State</TabButton>
-          </ul>
+          <menu>
+            <TabButton onSelect={() => handleSelect('components')}>
+              Components
+            </TabButton>
+            <TabButton onSelect={() => handleSelect('jsx')}>
+              JSX
+            </TabButton>
+            <TabButton onSelect={() => handleSelect('props')}>
+              Props
+            </TabButton>
+            <TabButton onSelect={() => handleSelect('state')}>
+              State
+            </TabButton>
+          </menu>
+
+          {tabContent}
         </section>
       </main>
     </div>
